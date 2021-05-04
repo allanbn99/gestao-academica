edb7f41f3f27a1b93e6af187286103c7988ac246
Elvisther Miranda <elvisther_cs@hotmail.com>
2021-05-04 15:34:53 -0400

    Enviando template

---
+++ routes/panel.php
@@ -0,0 +1,15 @@
+<?php
+
+/*-----------------------------------------------------------
+|   Rotas do painel
+-----------------------------------------------------------*/
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'painel'
+], function(){
+    Route::get('/', function(){ return view('painel.home'); })->name('panel.home');
+    Route::get('/cronograma', function(){ return view('painel.cronograma-aulas'); });
+});
+
+Route::get('/login', function(){ return view('painel.login'); });
