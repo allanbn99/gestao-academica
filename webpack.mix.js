0043f7441a821ce76219cb468286dca69e7244cf
Elvisther Miranda <elvisther_cs@hotmail.com>
2021-05-31 20:27:39 -0400

    Update

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .css('resources/css/app.css', 'public/css')
+    .css('resources/css/AdminSkyline.css', 'public/css')
+    .css('resources/css/LoginSkyline.css', 'public/css')
+    .css('resources/css/all.min.css', 'public/css')
     .sourceMaps();
