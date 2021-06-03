458bf25117f250bea5d13a24968d911d90ab6e6e
Elvisther Miranda <elvisther_cs@hotmail.com>
2021-06-03 19:41:48 -0400

    Deixando breadcumb dinamico

--- resources/views/painel/componentes/breadcumb.blade.php
+++ resources/views/painel/componentes/breadcumb.blade.php
@@ -1,6 +1,26 @@
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb bg-dark rounded-0">
-        <li class="breadcrumb-item"><a href="#" class="text-light">Home</a></li>
-        <!--<li class="breadcrumb-item active" aria-current="page">Cronograma de aulas</li>-->
+        <li class="breadcrumb-item"><a href="{{ route('panel.home') }}" class="text-light">Home</a></li>
+        @php
+            $uri = $_SERVER['REQUEST_URI'];
+            $ex = array_filter(explode('/', $uri));
+            $array = [];
+            for($i = 0;$i < count($ex);$i++){
+                array_push($array, $i);
+            }
+            function newKeyArray($key, $val){
+                return [$key => $val];
+            }
+            $result = array_map('newKeyArray', $array, $ex);
+        @endphp
+        @foreach ($result as $key => $value)
+            @if ($key != 0)
+                @if (array_key_last($result) == $key)
+                    <li class="breadcrumb-item active" aria-current="page">{{ $value[$key] }}</li>
+                @else
+                    <li class="breadcrumb-item text-light">{{ $value[$key] }}</li>
+                @endif
+            @endif
+        @endforeach
     </ol>
 </nav>
--- resources/views/painel/componentes/menu-vertical.blade.php
+++ resources/views/painel/componentes/menu-vertical.blade.php
@@ -5,17 +5,22 @@
     </div>
     
     <a href="#" class="sky-profile">
-        <img src="{{ asset('img/user1.jpg') }}" class="img-fluid"> Luís
+        <div class="d-flex flex-row">
+            <div class="mr-1 d-flex justify-content-center align-items-center"><img src="{{ asset('img/user1.jpg') }}" class="img-fluid"></div>
+            <div class="ml-1 d-flex align-items-center">
+                <div>Skyline be wolf<br><small class="text-muted">Técnico Administrativo</small></div>
+            </div>
+        </div>
     </a>
     
     <nav class="sky-navigator">
         <ul class="sky-navbar-vertical-drop">
             <li>
                 <a href="#" onclick="toggleMenu('drop1', 'caret1')">
-                    <i class="fas fa-user-graduate"></i> Manter Aluno <span class="float-right d-inline-block"><i id="caret1" class="fas fa-caret-down"></i></span>
+                    <i class="fas fa-user-graduate"></i> Secretaria <span class="float-right d-inline-block"><i id="caret1" class="fas fa-caret-down"></i></span>
                 </a>
                 <ul class="sky-dropdown" id="drop1">
-                    <li>
+                    <!--<li>
                         <a href="#" onclick="toggleMenu('drop2', 'caret2')">
                             <i class="fas fa-user-graduate"></i> Cadastrar <span class="float-right d-inline-block"><i id="caret2" class="fas fa-caret-down"></i></span>
                         </a>
@@ -30,22 +35,29 @@
                                 <a href="#">Sub-Item 3</a>
                             </li>
                         </ul>
+                    </li>-->
+                    <li>
+                        <a href="#">Aluno</a>
                     </li>
                     <li>
-                        <a href="#">Item 1</a>
+                        <a href="#">Funcionário</a>
                     </li>
                     <li>
-                        <a href="#">Item 1</a>
+                        <a href="#">Curso</a>
+                    </li>
+                    <li>
+                        <a href="#">Disciplina</a>
                     </li>
                 </ul>
             </li>
+        <!--
             <li>
                 <a href="#"><i class="fas fa-address-book"></i> Manter Aula</a>
             </li>
             <li>
                 <a href="#"><i class="fas fa-calendar"></i> Cronograma de aulas</a>
             </li>
-            
+        -->
             <li>
                 <a href="#"><i class="fas fa-sign-out-alt"></i> Sair</a>
             </li>
