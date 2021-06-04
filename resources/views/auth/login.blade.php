ae4da5994e6339d96ff334125adf8749b1aa8f78
Elvisther Miranda <elvisther_cs@hotmail.com>
2021-06-03 20:51:52 -0400

    Traduzido msgs e view login alterado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,49 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+	<title>Login</title>
+</head>
+<body class="bg-dark">
+	@if (Auth::check())
+		@php
+			echo redirect()->route('panel.home');
+		@endphp	
+	@endif
+	<div class="sky-container">
+		<div class="sky-login-box">
+			<div class="sky-login-header">
+				<img src="{{ asset('img/debian-logo.png') }}" class="img-fluid">
+			</div>
+			<div class="sky-login-body">
+				<form method="post" action="{{ route('login') }}">
+                    @csrf
+					<div class="input-group flex-nowrap mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text bg-dark border-dark text-light" id="addon-wrapping"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="email" class="form-control bg-dark border-dark text-light" value="{{ old('email') }}" name="email" placeholder="Usuário" aria-label="Username" aria-describedby="addon-wrapping">
+					</div>
+					<div class="input-group flex-nowrap mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text bg-dark border-dark text-light" id="addon-wrapping"><i class="fas fa-lock"></i></span>
+						</div>
+						<input type="password" class="form-control bg-dark border-dark text-light" name="password" placeholder="Senha" aria-label="Password" aria-describedby="addon-wrapping">
+					</div>
+					<button type="submit" class="btn btn-danger btn-block">Entrar</button>
+				</form>
+			</div>
+			<div class="warnings-login mt-4 mb-4 text-danger">
+                <!-- Validation Errors -->
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger">{{ $error }}</div>
+                @endforeach
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+		</div>
+	</div>
+	<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
